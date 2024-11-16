430f867f1477c9d0ab153e754261991dbb7b777d
hunter <2792843553@qq.com>
2024-11-16 22:19:59 +0800

    update leetcode 286

--- Algorithm/Practice/1.L386.cpp
+++ Algorithm/Practice/1.L386.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 void dfs(int current, int n) {
     if (current > n) return;
-    std::cout << current << " "; // 输出当前数字
+    cout << current << " "; 
     for (int i = 0; i <= 9; ++i) {
         int next = current * 10 + i;
         if (next <= n) {
-            dfs(next, n); // 递归调用
+            dfs(next, n); 
         }
     }
 }
 
 int main() {
     int n;
-    std::cin >> n;
+    cin >> n;
     for (int i = 1; i <= 9; ++i) {
-        dfs(i, n); // 从每个数字开始
+        dfs(i, n); 
     }
     return 0;
 }
