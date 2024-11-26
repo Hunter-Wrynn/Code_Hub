f9b0af64c81c7e66127b9986988bb91621e3b65a
Hunter <2792843553@qq.com>
2024-11-26 12:05:06 +0800

    add BCE

---
+++ Machine_Learning/Loss/Cross_Entropy.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+
+class BCELosswithlogits(nn.Module):
+    def __init__(self,pos_weight=1,reduction='mean'):
+        super(BCELosswithlogits,self).__init__()
+        self.pos_weight=pos_weight
+        self.reduction=reduction
+    
+    def forward(self,logits,target):
+        logits=F.sigmoid(logits)
+        loss=-self.pos_weight*target*torch.log(logits)-(1-target)*torch.log(1-logits)
+        
+        if self.reduction=='mean':
+            loss=loss.mean()     
+        elif self.reduction=='sum':
+            loss=loss.sum()    
+        return loss
