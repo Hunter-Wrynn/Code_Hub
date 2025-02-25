9579013238e1ee734bbb9945f71c1d7ffd20a9e6
Hunter-Wrynn <2792843553@qq.com>
2025-02-25 19:35:56 +0800

    update

--- Advanced_Transformer/Base_Transformer/Model/Encoder.py
+++ Advanced_Transformer/Base_Transformer/Model/Encoder.py
@@ -1,12 +1,12 @@
 import torch.nn as nn
 import torch
 import math
-from .FFN import PositinalWiseFeedForward
-from .Layer_Norm import LayerNorm
-from .Multihead_Attention import MultiheadAttention
-from .Positional_Embedding import PositionalEmbedding
-from .Token_Embedding import TokenEmbedding
-from .Transformer_Embedding import TransformerEmbedding
+from ..Layers.FFN import PositinalWiseFeedForward
+from ..Layers.Layer_Norm import LayerNorm
+from ..Layers.Multihead_Attention import MultiheadAttention
+from ..Layers.Positional_Embedding import PositionalEmbedding
+from ..Layers.Token_Embedding import TokenEmbedding
+from ..Layers.Transformer_Embedding import TransformerEmbedding
 
 class EncoderLayer(nn.Module):
     def __init__(self,d_model,ffn_hidden,n_head,drop_prob):
