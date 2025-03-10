265fa83b3820715a8ddf977a37fd61d1207cb477
Hunter <2792843553@qq.com>
2025-03-10 23:24:15 +0800

    add dpoloss

---
+++ Advanced_Transformer/Base_Transformer/Loss/dpo_loss.py
@@ -0,0 +1,18 @@
+def dpo_loss(ref_probs, probs, beta):
+    # ref_probs 和 probs 都是 shape: (batch_size, seq_len)
+    # 计算每个样本的平均概率
+    ref_probs = ref_probs.mean(dim=1)
+    probs = probs.mean(dim=1)
+
+    # 将 chosen 和 rejected 数据分开
+    batch_size = ref_probs.shape[0]
+    chosen_ref_probs = ref_probs[:batch_size // 2]
+    reject_ref_probs = ref_probs[batch_size // 2:]
+    chosen_probs = probs[:batch_size // 2]
+    reject_probs = probs[batch_size // 2:]
+
+    pi_logratios = chosen_probs - reject_probs
+    ref_logratios = chosen_ref_probs - reject_ref_probs
+    logits = pi_logratios - ref_logratios
+    loss = -F.logsigmoid(beta * logits)
+    return loss.mean()
