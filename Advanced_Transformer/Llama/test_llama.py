f8961c89f111d01b4acf6dc3b4b55b2d8513a9df
root <root@localhost.localdomain>
2025-07-05 10:21:38 -0400

    update llama

--- Advanced_Transformer/Llama/test_llama.py
+++ Advanced_Transformer/Llama/test_llama.py
@@ -1,21 +1,21 @@
-from transformers.models.llama import  LlamaModel,LlamaConfig
-
+from transformers.models.llama import LlamaModel, LlamaConfig
 import torch
 
-def main():
-    llamaConfig=LlamaConfig(vocab_size=32000,
-                            hidden_size=4096//2,
-                            intermediate_size=11008//2,
-                            num_hidden_layers=32//2,
-                            num_attention_heads=32//2,
-                            max_position_embedding=2048//2)
-    llamamodel=LlamaModel(config=llamaConfig)
-    
-    input_id=torch.randint(low=0,high=llamaConfig.vocab_size,size=(4,30))
-    
-    res=llamamodel(input_id)
-    print(res)
-    
-if __name__ == '__main__':
-    main()
+def run_llama():
+    llamaconfig = LlamaConfig(vocab_size=32000, hidden_size=4096//2,
+                              intermediate_size=11008//2,
+                              num_hidden_layers=32//2,
+                              num_attention_heads=32//2,
+                              max_position_embeddings=2048//2)
 
+    llamamodel = LlamaModel(config=llamaconfig)
+
+    inputs_ids = torch.randint(
+        low=0, high=llamaconfig.vocab_size, size=(4, 30))
+
+    res = llamamodel(inputs_ids)
+    print(res)
+
+
+if __name__ == '__main__':
+    run_llama()
