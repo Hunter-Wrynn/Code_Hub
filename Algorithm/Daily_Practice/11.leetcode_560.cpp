deaef36cab566c7c534854c648b09b5456af8ab4
Hunter-Wrynn <2792843553@qq.com>
2026-02-20 03:38:33 -0500

    lc 560-2

--- Algorithm/Daily_Practice/11.leetcode_560.cpp
+++ Algorithm/Daily_Practice/11.leetcode_560.cpp
@@ -13,3 +13,35 @@ class Solution {
             return count;
         }
     };
+
+class Solution2 {
+    public:
+        int subarraySum(vector<int>& nums, int k) {
+            int n = (int)nums.size();
+            int offset = 1000 * n;
+            int size = 2 * offset + 1;               // covers [-offset, +offset]
+    
+            vector<int> mp(size, 0);
+            mp[offset] = 1;                          // pre = 0
+    
+            int count = 0;
+            int pre = 0;
+    
+            for (int x : nums) {
+                pre += x;
+    
+                int need = pre - k;
+                int needIdx = need + offset;
+                if (0 <= needIdx && needIdx < size) {
+                    count += mp[needIdx];
+                }
+    
+                int preIdx = pre + offset;
+                // preIdx should always be in range given bounds, but keep safe if you want:
+                if (0 <= preIdx && preIdx < size) {
+                    mp[preIdx]++;
+                }
+            }
+            return count;
+        }
+    };
