637a7ec105da3b7462bd0d607eb70863c26b92ef
Hunter-Wrynn <2792843553@qq.com>
2026-02-20 03:34:27 -0500

    lc 560

---
+++ Algorithm/Daily_Practice/11.leetcode_560.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        int subarraySum(vector<int>& nums, int k) {
+            unordered_map<int,int> mp;
+            mp[0]=1;
+            int count=0;
+            int pre=0;
+            for(auto& x:nums){
+                pre+=x;
+                if(mp.find(pre-k)!=mp.end()) count+=mp[pre-k];
+                mp[pre]++;
+            }
+            return count;
+        }
+    };
